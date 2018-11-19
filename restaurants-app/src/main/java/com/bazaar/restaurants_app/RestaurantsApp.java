31651dd4b891db8be453fd760c2ce36982636f7f
Tomoki Lam <tomoki_lam@apple.com>
2018-11-19 17:35:16 +0800

    Updates

--- restaurants-app/src/main/java/com/bazaar/restaurants_app/RestaurantsApp.java
+++ restaurants-app/src/main/java/com/bazaar/restaurants_app/RestaurantsApp.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestaurantsApp {
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(RestaurantsApp.class);
+		System.out.println("hi");
 		app.setDefaultProperties(Collections.singletonMap("server.port", "8083"));
 		app.run(args);
 	}
